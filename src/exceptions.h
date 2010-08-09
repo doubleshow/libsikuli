564906db34f261655880664cd638e8d2542cd50f
Tom Yeh <doubleshow@gmail.com>
2010-08-09 12:39:27 -0400

    add exceptions.h

---
+++ src/exceptions.h
@@ -0,0 +1,57 @@
+/*
+ *  exceptions.h
+ *  sikuli
+ *
+ *  Created by Tom Yeh on 8/8/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _EXCEPTIONS_H_
+#define _EXCEPTIONS_H_
+
+#include <exception>
+
+namespace sikuli{
+   
+class FindFailed : public exception {
+   
+public:
+   FindFailed(string _msg) : msg(_msg) {};
+   FindFailed(Pattern _ptn) : ptn(_ptn) {};
+   
+   virtual const char* what() const throw(){
+      return msg.c_str();
+   }
+   
+   virtual ~FindFailed() throw() {};
+   
+private:
+   
+   string msg;
+   Pattern ptn;
+   
+};
+
+class FileNotFound : public exception {
+   
+public:
+   
+   FileNotFound(const char* filename) {
+      msg = string(filename) + " can not be found";
+   }
+   
+   virtual ~FileNotFound() throw() {};
+   
+   virtual const char* what() const throw(){
+      return msg.c_str();
+   }
+   
+private:
+   
+   string msg;
+};
+
+};
+
+#endif
