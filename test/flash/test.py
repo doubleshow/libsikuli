81dc7f35e29b69062d6aead75eefece37aacebe0
Tom Yeh <doubleshow@gmail.com>
2010-08-16 07:59:51 -0400

    add python test cases (except event manager)

---
+++ test/flash/test.py
@@ -0,0 +1,154 @@
+import unittest
+from sikuli import *
+import time
+
+Settings.addImagePath("images")
+click("SikuliTester.png")
+time.sleep(1)
+
+def switchToTest(x):
+    click(x)
+    time.sleep(1)
+
+
+class TestFlash(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        time.sleep(1)
+        
+    def test_click(self):
+        switchToTest("TestClick.png")
+        click("clickHere.png")
+        self.assertTrue(exists("success.png"))
+
+    def test_doubleClick(self):
+        switchToTest("TestDoubleClick.png")
+        doubleClick("clickHere.png")
+        self.assertTrue(exists("success.png"))
+
+    def test_rightClick(self):
+        rightClick("clickHere.png")
+        self.assertTrue(exists("about_adobe_flash_player.png"))
+        press(ESC)
+
+    def test_type(self):
+        switchToTest("TestType.png")
+        type("textinput.png","abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ !@#$%^&*()_+ 1234567890-=`~;':,./<>?\"[]{}\\|")
+        self.assertTrue(exists("success.png"))
+        
+    def test_keys(self):
+        switchToTest("TestKeys.png")
+        click("textinput.png");
+        press(DOWN)
+        press(UP)
+        press(LEFT)
+        press(RIGHT)
+        press(F1)
+        press(F2)
+        press(F3)
+        press(F4)
+        press(F5)
+        press(F6)
+        press(F7)
+        press(F8)
+        press(F9)
+        press(F10)
+        press(F11)
+        press(F12)
+        press(BACKSPACE)
+        press(DELETE)
+        press(END)
+        press(ENTER)
+        press(ESC)
+        press(PAGE_DOWN)
+        press(PAGE_UP)
+        press(SPACE)
+        press(CAPSLOCK)
+        press(INSERT)
+        press(HOME)
+        press(TAB)
+
+   
+    def test_OCR(self):
+        switchToTest("TestOCR.png")
+        initOCR("arial.png")
+        click("/System Preferences/")
+        click("/Configuration/")
+        click("/Keyboard/")
+        click("/My Favorites/")
+
+    def test_dragDrop(self):
+        switchToTest("TestDragDrop.png")
+        dragDrop("hslider_thumb.png", "dropHere.png")
+        self.assertTrue(exists("success.png"))
+
+    def test_findAll(self):
+        switchToTest("TestFindAll.png")
+        ms = findAll("flower.png")
+        self.assertEqual(len(ms), 6)
+
+        ms = findAll("folder.png")
+        self.assertEqual(len(ms), 5)
+
+        ms = findAll("checkbox.png")
+        self.assertEqual(len(ms), 5)
+
+        for m in ms:
+            click(m)
+
+        self.assertTrue(exists("success.png"))
+
+
+    def test_vanish(self):
+        switchToTest("TestVanish.png")
+        waitVanish("flower.png")
+        self.assertTrue(not exists("flower.png",0))
+
+    def test_spatialOp(self):
+        switchToTest("TestSpatialOp.png")
+      
+        computer = find("computer.png")
+        hover(computer)
+      
+        computer.left().find("clickButton.png").click(Location(10,10))
+        computer.below().find("clickButton.png").click(Location(10,10))
+        computer.right().find("clickButton.png").click(Location(10,10))
+        computer.above().find("clickButton.png").click(Location(10,10))
+      
+        self.assertTrue(exists("success.png"))
+
+
+    def test_ordering(self):
+        switchToTest("TestOrdering.png")
+
+        p = Pattern("button.png").leftMost()
+        click(p)
+        p = Pattern("button.png").bottomMost()
+        click(p)
+        p = Pattern("button.png").rightMost()
+        click(p)
+        p = Pattern("button.png").topMost()
+        click(p)
+      
+        p = Pattern("button.png").order(LEFTRIGHT)
+        click(findAll(p)[2]);
+        p = Pattern("button.png").order(BOTTOMUP)
+        click(findAll(p)[1]);
+        p = Pattern("button.png").order(RIGHTLEFT)
+        click(findAll(p)[2]);
+        p = Pattern("button.png").order(TOPDOWN)
+        click(findAll(p)[1])
+
+        self.assertTrue(exists("success.png"))
+      
+    def test_paste(self):
+        switchToTest("TestPaste.png")
+        paste("paste_target.png", "paSte tHis text here!!!")
+        self.assertTrue(exists("success.png"))
+  
+  
+  
+if __name__ == '__main__':
+    unittest.main()
